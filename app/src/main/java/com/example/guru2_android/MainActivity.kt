caed5d25b0ff1263c9e03f9647ae5024a929c492
hswest <precious01133@gmail.com>
2023-01-20 22:29:27 +0900

    날짜 선택

--- app/src/main/java/com/example/guru2_android/MainActivity.kt
+++ app/src/main/java/com/example/guru2_android/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.example.guru2_android
 
+import android.app.DatePickerDialog
+import android.icu.text.SimpleDateFormat
+import android.icu.util.Calendar
 import android.os.Bundle
 import android.view.Menu
+import android.view.View
+import android.widget.*
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -12,11 +17,20 @@ import androidx.navigation.ui.setupWithNavController
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.guru2_android.databinding.ActivityMainBinding
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var appBarConfiguration: AppBarConfiguration
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var date: TextView
+    private lateinit var selectDate: Button
+
+    private var selectDateBtn: Button? = null
+    private var dateTextView: TextView? = null
+    private var cal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,6 +56,36 @@ class MainActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+
+        date = findViewById(R.id.date)
+        selectDate = findViewById(R.id.selectDate)
+
+        dateTextView = this.date
+        selectDateBtn = this.selectDate
+
+        dateTextView!!.text = "----.--.--"
+
+        val dateSetListener = object : DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(view: DatePicker, year: Int, monthOfYear: Int,
+                                   dayOfMonth: Int) {
+                cal.set(Calendar.YEAR, year)
+                cal.set(Calendar.MONTH, monthOfYear)
+                cal.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+                updateDateInView()
+            }
+        }
+
+        selectDateBtn!!.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(view: View) {
+                DatePickerDialog(this@MainActivity,
+                    dateSetListener,
+                    // set DatePickerDialog to point to today's date when it loads up
+                    cal.get(Calendar.YEAR),
+                    cal.get(Calendar.MONTH),
+                    cal.get(Calendar.DAY_OF_MONTH)).show()
+            }
+        })
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -53,5 +97,11 @@ class MainActivity : AppCompatActivity() {
     override fun onSupportNavigateUp(): Boolean {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    private fun updateDateInView() {
+        val myFormat = "yyyy.MM.dd" // mention the format you need
+        val sdf = SimpleDateFormat(myFormat, Locale.US)
+        dateTextView!!.text = sdf.format(cal.getTime())
     }
 }
