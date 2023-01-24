3b30c269586967c21a032a3c1287376b777291ae
E1tanin <elt4nin@gmail.com>
2023-01-24 16:18:38 +0900

    mail icon delete

--- app/src/main/java/com/example/guru2_android/ui/mypage/MypageFragment.kt
+++ app/src/main/java/com/example/guru2_android/ui/mypage/MypageFragment.kt
@@ -1,4 +1,4 @@
-package com.example.guru2_android.ui.gallery
+package com.example.guru2_android.ui.mypage
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.guru2_android.databinding.FragmentGalleryBinding
+import com.example.guru2_android.databinding.FragmentMypageBinding
 
-class GalleryFragment : Fragment() {
+class MypageFragment : Fragment() {
 
-    private var _binding: FragmentGalleryBinding? = null
+    private var _binding: FragmentMypageBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -23,12 +23,12 @@ class GalleryFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         val galleryViewModel =
-            ViewModelProvider(this).get(GalleryViewModel::class.java)
+            ViewModelProvider(this).get(MypageViewModel::class.java)
 
-        _binding = FragmentGalleryBinding.inflate(inflater, container, false)
+        _binding = FragmentMypageBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textGallery
+        val textView: TextView = binding.myPageTextView
         galleryViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
