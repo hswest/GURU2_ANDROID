f761f83844d538213bd4476ec8531750aef477cb
박채연 <metrix02@naver.com>
2023-01-29 14:26:55 +0900

    //회원가입 페이지 기능 추가 + 코드 및 주석 정리

--- app/src/main/java/com/example/guru2_android/DBHelper.kt
+++ app/src/main/java/com/example/guru2_android/DBHelper.kt
@@ -25,7 +25,7 @@ class DBHelper(
     }
 
     override fun onCreate(db: SQLiteDatabase) {
-        // USER 라는 이름의 테이블을 생성하고 column 은 ID, PW, EMAIL 3개를 생성했습니다.
+        // USER 라는 이름의 테이블을 생성하고 column 은 ID, PW, EMAIL, NAME 4개를 생성했습니다.
         // UID 는 SQLite 를 사용하기 위해서 필수적으로 필요한 column
         var sql: String = "CREATE TABLE IF NOT EXISTS " +
                 "$TABLE_NAME ($UID integer primary key autoincrement, " +
@@ -40,27 +40,6 @@ class DBHelper(
         onCreate(db)
     }
 
-    // allUsers 리스트에 getter 를 만들어서 db 에 저장되어있는 모든 유저 정보를 가져옵니다.
-    val allUsers:List<User>
-        @SuppressLint("Range")
-        get() {
-            val users = ArrayList<User>()
-            val selectQueryHandler = "SELECT * FROM $TABLE_NAME"
-            val db = this.writableDatabase
-            val cursor = db.rawQuery(selectQueryHandler,null)
-            if(cursor.moveToFirst()){
-                do{
-                    val user = User()
-                    user.id = cursor.getString(cursor.getColumnIndex(COL_ID))
-                    user.pw = cursor.getString(cursor.getColumnIndex(COL_PW))
-                    user.email = cursor.getString(cursor.getColumnIndex(COL_EMAIL))
-
-                    users.add(user)
-                }while(cursor.moveToNext())
-            }
-            db.close()
-            return users
-        }
 
     fun checkIdExist(id: String): Boolean{
         val db = this.readableDatabase
@@ -86,12 +65,41 @@ class DBHelper(
         return cursor.count > 0
     }
 
+    fun checkEmailExist(email: String): Boolean{
+        val db = this.readableDatabase
+
+        // 리턴 받고자 하는 컬럼 값의 array
+        val projection = arrayOf(UID)
+
+        // where "id"=id
+        val selection= "$COL_EMAIL = ?"
+        val selectionArgs = arrayOf(email)
+
+        // 정렬조건 지정
+        val cursor = db.query(
+            TABLE_NAME, // 테이블
+            projection, // 리턴 받고자 하는 컬럼
+            selection, // where 조건
+            selectionArgs, // where 조건에 해당하는 값의 배열
+            null, // 그룹 조건
+            null, // having 조건
+            null // orderby 조건 지정
+        )
+        // 반환된 cursor 값이 존재하면 아이디 중복(true), 존재하지 않으면 아이디 생성가능(false)
+        return cursor.count > 0
+    }
+
     // db 에 새로운 유저를 추가하는 메소드(회원가입)
-    fun addUser(user: User){
+    fun addUser(user: User) {
         if(checkIdExist(user.id)) {
             Toast.makeText(this.context, "이미 존재하는 아이디 입니다.", Toast.LENGTH_SHORT).show()
             return
         }
+        if(checkEmailExist(user.email)) {
+            Toast.makeText(this.context, "이미 사용된 이메일입니다.", Toast.LENGTH_SHORT).show()
+            return
+        }
+
         val db = this.writableDatabase
         val values = ContentValues()
         values.put(COL_ID, user.id)
@@ -102,6 +110,7 @@ class DBHelper(
         db.insert(TABLE_NAME, null, values)
         db.close()
         Toast.makeText(this.context,"회원가입 성공", Toast.LENGTH_SHORT).show()
+
     }
     // 로그인 메소드
     @SuppressLint("Range")
@@ -127,27 +136,11 @@ class DBHelper(
         )
         if(cursor.moveToFirst()){ // 유저의 primary key(uid) 가져오기
             Log.d("test", cursor.getInt(cursor.getColumnIndex(UID)).toString())
+
         }
 
         return cursor.count > 0 // 반환 값이 존재하면 로그인 성공(true)
     }
-    // 유저 정보 업데이트 메소드
-    fun updateUser(user: User): Int{
-        val db = this.writableDatabase
-        val values = ContentValues()
-        values.put(COL_ID, user.id)
-        values.put(COL_PW, user.pw)
-        values.put(COL_EMAIL, user.email)
-        values.put(COL_NAME, user.name)
 
-        return db.update(TABLE_NAME, values, "$COL_ID=?", arrayOf(user.id))
-    }
 
-    // 유저 삭제 메소드
-    fun deleteUser(user: User){
-        val db = this.writableDatabase
-
-        db.delete(TABLE_NAME, "$COL_ID=?", arrayOf(user.id))
-        db.close()
-    }
 }
--- app/src/main/java/com/example/guru2_android/RegisterActivity.kt
+++ app/src/main/java/com/example/guru2_android/RegisterActivity.kt
@@ -1,40 +1,41 @@
 package com.example.guru2_android
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
+import android.widget.Toast
 import com.example.guru2_android.databinding.ActivityRegisterBinding
-import com.example.guru2_android.databinding.LoginBinding
 
 class RegisterActivity : AppCompatActivity() {
     lateinit var db: DBHelper
-    var users = ArrayList<User>()
     private lateinit var binding: ActivityRegisterBinding
-
-    lateinit var btnRegister: Button
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_register)
+        binding = ActivityRegisterBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         db = DBHelper(this)
 
-        btnRegister = findViewById<Button>(R.id.regiButton)
-
-        btnRegister.setOnClickListener {
+        binding.regiButton.setOnClickListener {
             inputUser().let {
                 if (it != null) {
                     db.addUser(it)
-
+                    val intent = Intent(this, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+                else {
+                    Toast.makeText(this, "정보를 모두 입력해주세요", Toast.LENGTH_SHORT).show()
                 }
             }
         }
     }
      private fun inputUser(): User? {
          val id = binding.id.text.toString()
-         val pw = binding.pw.toString()
-         val email = binding.email.toString()
-         val name = binding.name.toString()
+         val pw = binding.pw.text.toString()
+         val email = binding.email.text.toString()
+         val name = binding.name.text.toString()
 
          if (id == "" || pw == "" || email == "" || name == "") // 입력 정보가 하나라도 비어있으면
              return null // Null 반환
